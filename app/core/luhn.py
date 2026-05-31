3f7afb31d75538cb43d20fbb139151478df5ae8f
manu <manuelperezgargallo@gmail.com>
2026-05-31 20:22:33 +0100

    feature: Add generate_luhn function

--- app/core/luhn.py
+++ app/core/luhn.py
@@ -14,3 +14,22 @@ def validate_luhn(number: str) -> bool:
             checksum[i] -= 9
             
     return sum(checksum) % 10 == 0
+
+
+def generate_luhn(number: str) -> str:
+    """Generates the missing check digit for a base number using the Luhn Algorithm."""
+    if not number.isdigit():
+        return False
+    number += "0"    
+    digits = [int(d) for d in number]
+    # Reverse the digits to work from right to left
+    checksum = digits[::-1]
+    
+    # Double every second digit starting from the second to last
+    for i in range(1, len(checksum), 2):
+        checksum[i] *= 2
+        if checksum[i] > 9:
+            checksum[i] -= 9
+            
+    return str((10-sum(checksum) % 10) % 10)
+
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from app.core.luhn import validate_luhn
+from app.core.luhn import validate_luhn, generate_luhn
 
 app = FastAPI(title="Number Validator API")
 
@@ -17,3 +17,14 @@ def check_luhn(payload: NumberPayload):
         
     is_valid = validate_luhn(clean_number)
     return {"number": payload.number, "valid": is_valid}
+
+@app.post("/generate/luhn")
+def check_luhn(payload: NumberPayload):
+    # Stripping spaces or dashes if users input them formatted
+    clean_number = payload.number.replace(" ", "").replace("-", "")
+    
+    if not clean_number.isdigit():
+        raise HTTPException(status_code=400, detail="Input must contain only digits.")
+        
+    added_digit = generate_luhn(clean_number)
+    return {"new_number": payload.number+added_digit, "added_digit": added_digit}
